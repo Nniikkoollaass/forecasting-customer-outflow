8ffa6d66bb592b51abe9d361048dcd88fa93b52b
Maryana <154881614+lMaryanal@users.noreply.github.com>
2024-07-25 00:09:45 +0300

    First version

--- app.py
+++ app.py
@@ -4,12 +4,30 @@ from keras import models
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
+import xgboost as xgb
 
 
+#Фунція для обробки даних моделю та виводу результатів
+def model_predict(df, origin_df):
+    dmat = xgb.DMatrix(df)
+    pred = model.predict(dmat)
+    pred = np.round(pred).astype(int)
+    churn = pd.DataFrame(pred)
 
-df_minmax = pd.read_csv('minmax.csv')
-# def model_predict(client):
-#     predictions = model.predict(client)
+    churn.columns = ['pred_churn']
+    df = pd.concat([origin_df, churn], axis=1)
+    st.dataframe(df.to_dict('records'))
+
+    df_report = pd.DataFrame()
+    for id, client in df.iterrows():
+        row = pd.DataFrame({'id': [client['id']], 'pred_churn': [f"The customer has a {class_names[int(client['pred_churn'])]} probability of churn"]})
+
+        df_report = pd.concat([df_report, row], ignore_index=True)
+    st.dataframe(df_report.to_dict('records'))
+    # y = origin_df['churn']
+    # acc = accuracy_score(churn, y)
+    # st.write(acc)
+    return(pred)
 
 #Функція для перевірки даних, завантажених з файлу, на наявність всіх необхідних колонок та відповідність типу двних 
 def data_validation(df, rules):
@@ -20,7 +38,7 @@ def data_validation(df, rules):
             check = False
             continue
         
-        if not df[column].dropna().map(type).eq(types).all():
+        if not df[column].map(type).eq(types).all():#.dropna()
             st.error(f'Column {column} contains invalid data types.')
             check = False
             continue
@@ -32,15 +50,17 @@ def data_validation(df, rules):
     if not (df['is_tv_subscriber'].between(0, 1).all() and df['is_movie_package_subscriber'].between(0, 1).all()) :
         st.error(f'Values in column {column} are not in [0, 1].')
         check = False
+    
+    # if df.shape[1] != len(rules):
+    #     st.error(f'The number of columns {df.shape[1]} is incorrect.')
+    #     check = False
     return check
 
 # Функція для нормалізації
 def df_normal(df_original):
     df_concat = pd.concat([df_minmax, df_original], ignore_index=True)
-
     scaler = MinMaxScaler()
     df_concat[numeric_cols] = scaler.fit_transform(df_concat[numeric_cols])
-
     df = df_concat.drop([0, 1])
     return df
 
@@ -62,21 +82,8 @@ def add_data():
     df = pd.DataFrame([data])
     return df
 
-
-# df_rules = {
-#     'id': {'type': int},
-#     'is_tv_subscriber': {'type': int, 'nan': False},
-#     'is_movie_package_subscriber': {'type': int, 'nan': False},
-#     'subscription_age': {'type': float, 'nan': False},
-#     'bill_avg': {'type': int, 'nan': False},
-#     'reamining_contract': {'type': float, 'nan': True},
-#     'service_failure_count': {'type': int, 'nan': False},
-#     'download_avg': {'type': float, 'nan': True},
-#     'upload_avg':{'type': float, 'nan': True},
-#     'download_over_limit': {'type': int, 'nan': False}
-# }
 numeric_cols = ['subscription_age', 'bill_avg', 'reamining_contract', 'service_failure_count','download_avg', 'upload_avg'] #'download_over_limit'
-
+class_names = ['low', 'high']
 #Зразок типів даних в колонках
 df_type = {
     'id': int,
@@ -92,8 +99,13 @@ df_type = {
 }
  
 #Завантаження моделі
-#model = models.load_model('model.h5')
+#model = models.load_model('keras_model.h5')
+model = xgb.Booster()
+model.load_model('xgb_model.json')
 
+#дані для нормалізації
+df_minmax = pd.read_csv('minmax.csv')
+  
 st.title('Predicting customer churn for a telecoms company')
 
 # Вибір між файлом та введенням даних
@@ -105,72 +117,60 @@ if input_type == 'Upload the file':
 
     if uploaded_file is not None:
         # Відображення назви завантаженого файлу
-
         st.write("File name:", uploaded_file.name)
-
         # Читання файлу
         df_file = pd.read_csv(uploaded_file)
         if df_file.empty:
             st.warning("The uploaded CSV file is empty.")
         else:
-            st.write(df_file)
-            df = df_file
-
-            # заміна порожніх значень 'reamining_contract' на 0
-            if 'reamining_contract' in df.columns:
-                df['reamining_contract'] = df['reamining_contract'].fillna(0)
-
-            #Перевірка коректності даних. 
-            if data_validation(df, df_type):
-
-                #Попередня обробка даних
-                df.drop('id', axis=1, inplace=True)
-
-                df = df_normal(df)
+            st.dataframe(df_file.to_dict('records'))
             
+            if st.button("predict"):
+                df = df_file
+                # заміна порожніх значень 'reamining_contract' на 0
+                if 'reamining_contract' in df.columns:
+                    df['reamining_contract'] = df['reamining_contract'].fillna(0)
 
-                #Прогнозування моделі та вивід результатів
-                #model_predict(df)
+                #Перевірка коректності даних. 
+                if data_validation(df, df_type):
+                    if df.shape[1] > len(df_type):
+                        df = df[df_type.keys()]
+
+                    #Попередня обробка даних
+                    df.drop('id', axis=1, inplace=True)
+                    df = df_normal(df)
+
+                    df.drop('service_failure_count', axis=1, inplace=True)
+                    df.drop('download_over_limit', axis=1, inplace=True)
+                    #Прогнозування моделі та вивід результатів
+                    model_predict(df, df_file)
 
 elif input_type == 'Enter from the keyboard':
-    
-    
     id = st.number_input('id:', step=1, min_value=1, key='id')
-
-    col11, col12, col13 = st.columns(3)
-
     # створення трьох колонок
+    col11, col12, col13 = st.columns(3)
     with col11:
        is_tv_subscriber = st.radio('is_tv_subscriber', [0, 1], key='is_tv_subscriber')
-
     with col12:
        is_movie_package_subscriber = st.radio('is_movie_package_subscriber', [0, 1], key='is_movie_package_subscriber')
-
     with col13:
        subscription_age =  st.number_input('subscription_age:', min_value=0.00, value=0.00, key='subscription_age')
 
     col21, col22, col23 = st.columns(3)
-
     with col21:
        bill_avg = st.number_input('bill_avg:', step=1, min_value=0, key='bill_avg')
-        
     with col22:
        reamining_contract = st.number_input('reamining_contract:', min_value=0.00, value=0.00, key='reamining_contract')
-
     with col23:
        service_failure_count = st.number_input('service_failure_count:', step=1, min_value=0, key='service_failure_count')
     
     col31, col32, col33 = st.columns(3)
-       
     with col31:
        download_avg = st.number_input('download_avg:', min_value=0.00, value=0.00, key='download_avg')
-
     with col32:
        upload_avg = st.number_input('upload_avg:', min_value=0.00, value=0.00, key='upload_avg')
-
     with col33:
        download_over_limit = st.number_input('download_over_limit:', step=1, min_value=0, key='download_over_limit')
-    
 
     # Кнопка для додавання даних
     if st.button("add a client"):
@@ -197,32 +197,30 @@ elif input_type == 'Enter from the keyboard':
     #Вивід даних
     if 'dataframe' in st.session_state:
         st.write("Current data:")
-        st.write(st.session_state['dataframe'])
-
-    
-
+        st.dataframe(st.session_state['dataframe'].to_dict('records'))
 
     if st.button("predict"):
         try:
             df_input = st.session_state['dataframe']
             df = df_input
-            #Попередня обробка даних
-            df.drop('id', axis=1, inplace=True)
-            df = df_normal(df)
-            #st.write(df)
+ 
+            if data_validation(df, df_type):
+                df = df.fillna(0)
+                #Попередня обробка даних
+                df.drop('id', axis=1, inplace=True)
+                df = df_normal(df)
+                df.drop('service_failure_count', axis=1, inplace=True)
+                df.drop('download_over_limit', axis=1, inplace=True)
 
-            #Обробка моделлю
-
-            # predictions = models_dic[type_model].predict(processed_image)
-            # predicted_class = np.argmax(predictions)
-
-            # # Вивід результатів
-            # st.write("### Результати класифікації:")
-            # st.write(f'Predicted class name: {class_names[predicted_class]}')
-            # for label, prob in zip(class_names, predictions[0]):
-            #     st.write(f"{label}: {prob * 100:.2f}%")
+                #Обробка моделлю
+                model_predict(df, df_input)
         except:
             st.warning("Please add the data.")
+        
+
+
+
+        
 
 
 
